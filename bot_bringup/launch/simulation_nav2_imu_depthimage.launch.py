8175267c8c93cf7203674176853b7fb6e4c4fca4
hurryzy <jeladeer@msn.com>
2025-12-11 09:42:22 +0800

    rtf 现在基本稳定了，主要是"libgazebo_ros_camera.so点云计算太费，所以现在关掉了极小参数设定，不让点云触发

--- bot_bringup/launch/simulation_nav2_imu_depthimage.launch.py
+++ bot_bringup/launch/simulation_nav2_imu_depthimage.launch.py
@@ -301,7 +301,7 @@ def launch_setup(context: LaunchContext, *args, **kwargs):
             
             # Scan configuration / 扫描配置
             'scan_height': 10,              # Number of pixel rows to use / 使用的像素行数
-            'scan_time': 0.067,             # Time between scans: 1/15Hz = 0.067s / 扫描间隔(匹配相机15Hz)
+            'scan_time': 0.1,               # Time between scans: 1/10Hz = 0.1s / 扫描间隔(匹配相机10Hz)
             
             # Range limits / 范围限制
             'range_min': 0.05,              # Min range 5cm for close detection / 最小范围5cm用于近距离检测
