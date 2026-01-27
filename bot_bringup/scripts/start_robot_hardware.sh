49f0e282848cada0cded962f886878207707add6
hurryzy <jeladeer@msn.com>
2026-01-27 14:09:18 +0800

    update shell

--- bot_bringup/scripts/start_robot_hardware.sh
+++ bot_bringup/scripts/start_robot_hardware.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-cd ../../../
-source /opt/ros/humble/setup.bash
-source install/setup.bash
-
 # 启动硬件驱动节点
 ros2 launch bot_hardware hardware_bringup.launch.py \
   enable_servo:=true \
--- bot_bringup/scripts/start_robot_navigation.sh
+++ bot_bringup/scripts/start_robot_navigation.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-
-cd ../../../
-source install/setup.bash
-
 # 启动导航和SLAM节点
 ros2 launch bot_bringup remote_navigation.launch.py \
   slam:=true \
